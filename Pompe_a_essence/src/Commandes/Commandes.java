c7a18a46c26b4a644f03b91c2c4e1eae50c429ca
Rubenson Cherenfant <92955778+CherenfantRubenson@users.noreply.github.com>
2023-03-08 14:14:06 -0500

    Update Commandes.java

--- Pompe_a_essence/src/Commandes/Commandes.java
+++ Pompe_a_essence/src/Commandes/Commandes.java
@@ -1,95 +1,319 @@
 package Commandes;
-
-public class Commandes {
-	 
-    private  int ID;
-    private int quantiteGallonDiesel;
-    private int QuantiteGallonGazoline;
-    private String dateCommande;
-    private String etat;
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public int getQuantiteGallonDiesel() {
-        return quantiteGallonDiesel;
-    }
-
-    public void setQuantiteGallonDiesel(int quantiteGallonDiesel) {
-        this.quantiteGallonDiesel = quantiteGallonDiesel;
-    }
-
-    public int getQuantiteGallonGazoline() {
-        return QuantiteGallonGazoline;
-    }
-
-    public void setQuantiteGallonGazoline(int QuantiteGallonGazoline) {
-        this.QuantiteGallonGazoline = QuantiteGallonGazoline;
-    }
-
-    public String getDateCommande() {
-        return dateCommande;
-    }
-
-    public void setDateCommande(String dateCommande) {
-        this.dateCommande = dateCommande;
-    }
-
-    public String getEtat() {
-        return etat;
-    }
-
-    public void setEtat(String etat) {
-        this.etat = etat;
-    }
-
-    
-    
-    public Commandes() {
-    }
-
+// package projetfinaljava;
+// import java.time.LocalDate;
+// import java.util.Scanner;
+// import static projetfinaljava.Stations.tab;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+/**
  
-    
-  public  ArrayList<String> Enregistrer() {
-    ArrayList<String> commandesList = new ArrayList<>();
-    Commandes  com = new Commandes();
-    
-    Scanner myobject = new Scanner(System.in);
-    
-    // Ajouter les valeurs des attributs à l'ArrayList
-    System.out.println("Entrer l'ID du commande");
-    
-     com.setID(myobject.nextInt(ID));
-    
-  
-    
-    com.setQuantiteGallonDiesel(myobject.nextInt(quantiteGallonDiesel));
-    
-   
-    
-    
-    
-    commandesList.add(Integer.toString(ID));
-    commandesList.add(Integer.toString(quantiteGallonDiesel));
-    commandesList.add(Integer.toString(QuantiteGallonGazoline));
-    commandesList.add(dateCommande);
-    
-    
-    return commandesList;
-}
-  
-  public void afficher() {
-    ArrayList<String> commandesList = Enregistrer();
-    System.out.println("ID: " + commandesList.get(0));
-    System.out.println("Quantité de Gallons de Diesel: " + commandesList.get(1));
-    System.out.println("Quantité de Gallons de Gazoline: " + commandesList.get(2));
-    System.out.println("Date de Commande: " + commandesList.get(3));
-    System.out.println("Etat: " + commandesList.get(4));
-}
+ */
 
+public class Commandes
+{
+
+    //private static Stations[] Stations;
+    private int comGazo,comDie;
+    private String id, dateCom, etat;
+    //static Stations tab[]=new Stations[2];
+    Stations stat=new Stations();
+    Scanner clav=new Scanner(System.in);
+    Scanner clavStri=new Scanner(System.in);
+    Commandes []tabCom=new Commandes[3];
+    public Commandes(){this.etat = "N";}
+    
+    
+    public Commandes(String id, int comGazo, int comDie, String dateCom)
+    {
+        this.id = id;
+        this.comGazo = comGazo;
+        this.comDie = comDie;
+        this.dateCom = dateCom;
+        this.etat = "N";
+    }
+    
+//Les getters et les setters
+    public int getComGazo() {
+        return comGazo;
+    }
+
+    public void setComGazo(int comGazo) {
+        this.comGazo = comGazo;
+    }
+
+    public int getComDie() {
+        return comDie;
+    }
+
+    public void setComDie(int comDie) {
+        this.comDie = comDie;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDateCom() {
+        return dateCom;
+    }
+
+    public void setDateCom(String dateCom) {
+        this.dateCom = dateCom;
+    }
+
+
+////////////////////////////Les Mehodes\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    void command()
+    {
+        int choix=0;
+        int somGazo=0,somDiel=0;
+        for(int i=0 ; i<tab.length; i++)
+            
+        {
+           somGazo+=((tab[i].getCapaciteGazo()-tab[i].getQuantiteGazoD())*1.25);
+           somDiel+=((tab[i].getCapaciteDies()-tab[i].getQuantiteDiesDis())*1.10);
+        }
+         
+//   int prGazD=0;
+        String rep="";
+           // int prGaz=0;
+          int i=0;
+        //for(int i=0; i<tabCom.length; i++)
+        //{
+       // do{
+            //int i=0;
+            //tabCom[i]=new Commandes();
+         //Commandes com=new Commandes();
+        
+
+           do{ 
+              String pass="";
+              /*System.out.println("1-. Pour Gazoline");
+               System.out.println("2-. Pour Diesel");
+               System.out.println("3-. Pour Gazoline et Diesel");
+               System.out.println("Choix !!!");
+              */
+               tabCom[i]=new Commandes();
+               choix=verifyeGD();
+              System.out.println("Entrer votre mot de passe");
+              pass=clavStri.nextLine();
+              int co=0;
+              while(!pass.equals("Authentic"))
+              { 
+                co+=1;
+                                
+
+                  if(co==3)
+                  {
+                      break;
+                  }
+                  System.out.println("Mot de passe incorrect !" + "\n"+"Entrer votre mot de passe");
+                  pass=clavStri.nextLine();
+                  //co+=1;
+               }
+              if(co!=3){
+                switch(choix)
+                {
+                    case 1:
+                    {
+                        //
+                        System.out.println("La commande du gazoline est effectuee avec succes");
+                       // prGaz=somGazo;
+                        tabCom[i].comGazo=somGazo;
+
+                    }break;
+
+                    case 2:
+                    {
+                        System.out.println("La commande du succes est effectuee avec succes");  
+                        //prGazD=somDiel;
+                        tabCom[i].comDie=somDiel;
+                                              
+                    }break;
+                    
+                    case 3:
+                    {
+                        System.out.println("La commande des deux gaz sont effectuees avec succes"); 
+                      //  prGaz=somGazo;
+                        //prGazD=somDiel;        
+                              tabCom[i].comGazo=somGazo;
+                              tabCom[i].comDie=somDiel;
+                    }break;
+                }    
+              
+              }
+               
+           if(i>0){
+               if(i==1){
+                   tabCom[i-1].etat="P";
+               }
+               if(i==2){
+                 tabCom[i-1].etat="P";
+                 tabCom[i-2].etat="P";
+               }
+           }
+            tabCom[i].id=(i+1000)+"$";
+            LocalDate date = LocalDate.now();
+            // Convertir la date en une chaîne de caractères
+            String dateString = date.toString();
+            tabCom[i].dateCom=dateString; 
+            i+=1;
+            System.out.println("O pour continuer N pour annuler");
+            rep=clavStri.nextLine();
+           }while(rep.equalsIgnoreCase("O"));
+
+        }
+           
+
+
+        //Pour les affichers
+    void afficherCommand()
+    {
+        System.out.println("Voici la liste des commandes ");
+        for(Commandes s:tabCom)
+        {//{
+            //System.out.println(v.getNom());
+            if(s==null)
+            {             
+                System.out.println();
+            }
+            else{
+               System.out.println(s.toString()); 
+               System.out.println();
+            }
+        }
+            //if(s.getComGazo()!=0){
+            //System.out.println(s.toString());
+            //System.out.println();
+            //}else{
+            //System.out.println(s.toString());
+            //System.out.println();
+             //System.out.println("wwwwwwwwww");       
+       // }
+        
+    }
+    
+    
+    void supprimer()
+    {
+              //choix=clav.nextInt();
+              String pass="";
+              System.out.println("Entrer votre mot de passe");
+              pass=clavStri.nextLine();
+              int co=0;
+              while(!pass.equals("Authentic"))
+              { 
+                co+=1;
+                                
+
+                  if(co==3)
+                  {
+                      break;
+                  }
+                  System.out.println("Mot de passe incorrect !" + "\n"+"Entrer votre mot de passe");
+                  pass=clavStri.nextLine();
+                  //co+=1;
+               }
+              if(co!=3){
+
+		String codeSai;
+		System.out.println("Entrer code du produit que vous voulez supprimer ");
+		codeSai=clavStri.nextLine();
+		for(int i=0; i<tabCom.length; i++)
+		{
+
+			if(tabCom[i].id.equalsIgnoreCase(codeSai))
+			{
+				tabCom[i].id="S";	
+				tabCom[i].comGazo=0;		
+				tabCom[i].comDie=0;	
+				tabCom[i].dateCom="";
+                                tabCom[i].etat="";
+			}
+                }
+			//for(int i=0; i<tabCom.length; i++)
+                        for(Commandes c : tabCom)
+			{
+                            
+				//if(!tabCom[i].id.equalsIgnoreCase("S"))
+                            if(c.getId().equalsIgnoreCase("S") || c==null)
+				{
+					//System.out.println(tabCom[i].toString());
+                                        System.out.println();
+				}
+                            else
+                            {
+                                System.out.println(c.toString());
+                            }
+			}	
+              }
+    }
+
+    @Override
+    public String toString() {
+        return    "Le code du commande                 =" + id+"\n" 
+                + "Qnte Gazoline commande              =" + comGazo+" Gallon(s)\n"
+                + "Qnt Diesel commande                 =" + comDie+" Gallon(s)\n"
+                + "La date du commande                 =" + dateCom+"\n" 
+                + "L'etat du commande                  =" + etat;
+    }
+    
+
+            static int verifyeGD(){	
+	Scanner ent=new Scanner(System.in);
+	int num=0;
+             boolean validInput = false;
+
+        while (!validInput) {
+               System.out.println("1-. Pour Gazoline");
+               System.out.println("2-. Pour Diesel");
+               System.out.println("3-. Pour Gazoline et Diesel");
+               System.out.println("Choix !!!");
+                String input = ent.nextLine();
+		boolean isNumber = true;
+            for (int a = 0; a < input.length(); a++) {
+                if (!Character.isDigit(input.charAt(a))) {
+                    isNumber = false;
+                    break;
+                }
+				
+            }
+		if (isNumber)
+                {
+                    num = Integer.parseInt(input);
+                    if (num<1 || num>3) {
+                     System.out.println("Erreur : vous devez entrer un nombre entier compris entre 1 a 3 "); 
+                     //System.out.println("Select le nom du Station \n1.--------Tabarre\n2.--------Petion-Ville\n3.--------Mirebalais\n4.--------Gressier");
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+
+		}		
+		
+                return num;	
+		
+	}
+    
+    //*/
+    
+
+   public static void main(String args[])
+   {
+       System.out.println("Welcome in my program");
+       Commandes insCom=new Commandes();
+       Stations stat=new Stations();
+       stat.Saisir();
+       
+       insCom.command();
+       insCom.afficherCommand();
+       insCom.supprimer();
+    }
+}
 }
